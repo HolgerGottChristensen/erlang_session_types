fce107b4a958581fb86d64567bd66a350ef25937
holgerchristensen <holger.gott@icloud.com>
2022-11-10 14:05:59 +0100

    Update

--- src/hello_world.erl
+++ src/hello_world.erl
@@ -15,7 +15,8 @@
 
 -register([
   {hello1, id1},
-  {hello2, id2}
+  {hello2, id2},
+  {hello3, id3}
 ]).
 
 -session({{id1, id2}, [
@@ -25,16 +26,24 @@
   eot
 ]}).
 
+-session({{id1, id3}, [
+  {send, integer},
+  eot
+]}).
+
 main() ->
   PID1 = spawn(?MODULE, hello1, []),
   PID2 = spawn(?MODULE, hello2, []),
+  PID3 = spawn(?MODULE, hello3, []),
   register(id1, PID1),
-  register(id2, PID2).
+  register(id2, PID2),
+  register(id3, PID3).
 
 
 hello1() ->
   id2 ! {id1, 42},
   id2 ! {id1, 42},
+  id3 ! {id1, 42},
   receive {id2, Val} when is_integer(Val) ->
     io:fwrite("Forms = ~p~n", [Val])
   end.
@@ -47,6 +56,11 @@ hello2() ->
     io:fwrite("Forms = ~p~n", [Val2])
   end,
   id1 ! {id2, 42}.
+
+hello3() ->
+  receive {id1, Val} when is_integer(Val) ->
+    io:fwrite("Forms = ~p~n", [Val])
+  end.
 
 parse() ->
   {ok, AST} = epp:parse_file("src/hello_world.erl", [{includes, "include"}]),
